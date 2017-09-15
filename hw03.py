03175e1ba1e720fa7ee2c672d7f9bbde13695367
TerryMinal <tguan@stuy.edu>
2017-09-15 13:17:18 -0400

    finished load_file

--- hw03.py
+++ hw03.py
@@ -6,24 +6,36 @@
 import random
 
 #A list with total % * 10 entries, each occupation has its percentage * 10 entries
-occupations = []
+occupations10 = []
 
-#text of the .csv file
-file_read = ""
+occupations = []
 
 #function to load the information from the file
 def load_file(filename):
-    #    file_open = open(filename)
-    #   file_read = file_open.read()
-    #  add_entries()
     with open (filename, 'r') as f:
         fstr = f.read()
         list_split = fstr.split('\n')
+        #print list_split
         heading = list_split[0]
         list_split = list_split[1:len(list_split) - 1]
-        list_split = [item.split(',') for item in list_split]
-  print list_split
-    
+        temp = []
+        for item in list_split:
+            if item[0] == '"':
+                temp2 = []
+                temp2.append(item[1:item.index('"', 1)])
+                temp2.append(float(item[item.index('",', 1) + 2 :]))
+                temp.append(temp2)
+            else:
+                temp.append(item.split(","))
+                temp[len(temp) - 1][1] = float(temp[len(temp) - 1][1])
+        occupations = temp[:len(temp) - 2]
+        #print occupations
+                
+def load_occupations():
+    for item in occupations:
+        for i in range(item[1] * 10):
+            occupations10.append(item[0])
+    print occupations10
 #
 def get_random():
     return occupations[random.randint(0,len(occupations) - 1)]
