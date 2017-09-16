d65974f6e3b2c1138528ddf49dfd6bbbe2bf1521
Naotaka Kinoshita <naotakakinoshita@gmail.com>
2017-09-15 20:05:09 -0400

    everything done

--- hw03.py
+++ hw03.py
@@ -1,46 +1,49 @@
-# Terry Guan and Naotaka Kinoshita
-# soft_dev1 pd7
-# HW03 -- StI/O: Divine your Destiny!
-# 2017-09-14
+### Terry Guan and Naotaka Kinoshita
+### soft_dev1 pd7
+### HW03 -- StI/O: Divine your Destiny!
+### 2017-09-14
 
 import random
 
-#A list with total % * 10 entries, each occupation has its percentage * 10 entries
+## A list with total % * 10 entries, each occupation has its percentage * 10 entries
 occupations10 = []
 
+## list of occupations with percentages attached
 occupations = []
 
-#function to load the information from the file
+## function to load the information from the file
 def load_file(filename):
     with open (filename, 'r') as f:
+        ## Read fileand make a list
         fstr = f.read()
         list_split = fstr.split('\n')
-        #print list_split
         heading = list_split[0]
         list_split = list_split[1:len(list_split) - 1]
         temp = []
+        ## Accounting for occupations with commas in them
         for item in list_split:
             if item[0] == '"':
                 temp2 = []
                 temp2.append(item[1:item.index('"', 1)])
-                temp2.append(float(item[item.index('",', 1) + 2 :]))
+                temp2.append(float(item[item.index('",', 1) + 2 :])) ## adding percentages and converting to float
                 temp.append(temp2)
             else:
                 temp.append(item.split(","))
                 temp[len(temp) - 1][1] = float(temp[len(temp) - 1][1])
+        global occupations
         occupations = temp[:len(temp) - 2]
-        #print occupations
-                
+
+## Add occupations by their percentages ex:6.1% becomes 61 entries
 def load_occupations():
     for item in occupations:
         for i in range(int(item[1] * 10)):
             occupations10.append(item[0])
-    print occupations10
-#
+    #print occupations10
+
+## returns random element from occupations10
 def get_random():
-    return occupations[random.randint(0,len(occupations) - 1)]
+    return occupations10[random.randint(0,len(occupations10) - 1)]
 
 load_file("occupations.csv")
-
 load_occupations()
-#print (get_random)
+print get_random()
